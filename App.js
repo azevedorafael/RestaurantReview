4c73efad93c4f658780e22456898350cecb50faa
azevedorafael <rafaelsilva.fatec@gmail.com>
2019-04-16 22:36:13 -0300

    Add ScrollView

--- App.js
+++ App.js
@@ -7,14 +7,38 @@
  */
 
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View, TextInput, ScrollView } from 'react-native';
 
 import Header from 'components/Header'
 
 const restaurants = [
   { name: 'React Cafe', address: '123 AN' },
   { name: 'Fancy Restaurant', address: '799 Main St' },
-  { name: 'Taco Place', address: '550 Maple Rd' }
+  { name: 'Taco Place', address: '550 Maple Rd' },
+  { name: 'React Cafe', address: '123 AN' },
+  { name: 'Fancy Restaurant', address: '799 Main St' },
+  { name: 'Taco Place', address: '550 Maple Rd' },
+  { name: 'React Cafe', address: '123 AN' },
+  { name: 'Fancy Restaurant', address: '799 Main St' },
+  { name: 'Taco Place', address: '550 Maple Rd' },
+  { name: 'React Cafe', address: '123 AN' },
+  { name: 'Fancy Restaurant', address: '799 Main St' },
+  { name: 'Taco Place', address: '550 Maple Rd' },
+  { name: 'React Cafe', address: '123 AN' },
+  { name: 'Fancy Restaurant', address: '799 Main St' },
+  { name: 'Taco Place', address: '550 Maple Rd' },
+  { name: 'React Cafe', address: '123 AN' },
+  { name: 'Fancy Restaurant', address: '799 Main St' },
+  { name: 'Taco Place', address: '550 Maple Rd' },
+  { name: 'React Cafe', address: '123 AN' },
+  { name: 'Fancy Restaurant', address: '799 Main St' },
+  { name: 'Taco Place', address: '550 Maple Rd' },
+  { name: 'React Cafe', address: '123 AN' },
+  { name: 'Fancy Restaurant', address: '799 Main St' },
+  { name: 'Taco Place', address: '550 Maple Rd' },
+  { name: 'React Cafe', address: '123 AN' },
+  { name: 'Fancy Restaurant', address: '799 Main St' },
+  { name: 'Taco Place', address: '550 Maple Rd' },
 ]
 
 export default class App extends Component {
@@ -38,34 +62,39 @@ export default class App extends Component {
           value={this.state.search}
         />
 
-        {
-          restaurants
-          .filter(place => {
-            return !this.state.search ||
-              place.name.toLowerCase().indexOf(this.state.search.toLowerCase()) > -1
-          })
-          .map((place, index) => {
-            return (
-              <View key={place.name} style={[
-                styles.row,
-                { backgroundColor: index % 2 === 0 ? 'white' : '#F3F3F7' }
-              ]}>
-                <View style={styles.edges}>
-                  <Text>{index + 1}</Text>
-                </View>
+        <ScrollView contentContainerStyle={{
+          paddingTop: 30
+        }}>
+          {
+            restaurants
+              .filter(place => {
+                return !this.state.search ||
+                  place.name.toLowerCase().indexOf(this.state.search.toLowerCase()) > -1
+              })
+              .map((place, index) => {
+                return (
+                  <View key={place.name} style={[
+                    styles.row,
+                    { backgroundColor: index % 2 === 0 ? 'white' : '#F3F3F7' }
+                  ]}>
+                    <View style={styles.edges}>
+                      <Text>{index + 1}</Text>
+                    </View>
 
-                <View style={styles.nameAddress}>
-                  <Text>{place.name}</Text>
-                  <Text style={styles.addressText}>{place.address}</Text>
-                </View>
+                    <View style={styles.nameAddress}>
+                      <Text>{place.name}</Text>
+                      <Text style={styles.addressText}>{place.address}</Text>
+                    </View>
 
-                <View style={styles.edges}>
-                  <Text>Info</Text>
-                </View>
-              </View>
-            )
-          })
-        }
+                    <View style={styles.edges}>
+                      <Text>Info</Text>
+                    </View>
+                  </View>
+                )
+              })
+          }
+        </ScrollView>
+
       </View>
     );
   }
@@ -88,7 +117,6 @@ const styles = StyleSheet.create({
     'color': 'grey'
   },
   input: {
-    marginBottom: 30,
     padding: 10,
     paddingHorizontal: 20,
     fontSize: 16,
